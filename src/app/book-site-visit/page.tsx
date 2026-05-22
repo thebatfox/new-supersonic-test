9c4ea7281e7f2d21d392ab3215bc8ffff097a6fa
Supersonic Deploy <deploy@supersoniccustoms.co.za>
2026-05-22 21:15:27 +0000

    Update time slots: 09:00-16:00

--- src/app/book-site-visit/page.tsx
+++ src/app/book-site-visit/page.tsx
@@ -34,11 +34,10 @@ const initialData: SiteVisitData = {
 };
 
 const timeSlots = [
-  "08:00 - 10:00",
-  "10:00 - 12:00",
-  "12:00 - 14:00",
-  "14:00 - 16:00",
-  "16:00 - 18:00"
+  "09:00 - 11:00",
+  "11:00 - 13:00",
+  "13:00 - 15:00",
+  "15:00 - 16:00"
 ];
 
 export default function BookSiteVisitPage() {
